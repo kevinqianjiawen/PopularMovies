fba90c7da6e68ee9384e06ba2c9666b8819d4fe2
kevin <kevin_qianjiawen@hotmail.com>
2016-08-06 21:17:48 -0400

    create database

---
+++ app/src/main/java/com/example/android/popularmovies/data/MovieDBHelper.java
@@ -0,0 +1,50 @@
+package com.example.android.popularmovies.data;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class MovieDBHelper extends SQLiteOpenHelper {
+	public static final String LOG_TAG = MovieDBHelper.class.getSimpleName();
+
+	//name & version
+	private static final String DATABASE_NAME = "movie.db";
+	private static final int DATABASE_VERSION = 12;
+
+	public MovieDBHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	// Create the database
+	@Override
+	public void onCreate(SQLiteDatabase sqLiteDatabase) {
+		final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " +
+				MovieContract.MovieEntry.TABLE_NAME + "(" + MovieContract.MovieEntry._ID +
+				" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+				MovieContract.MovieEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+				MovieContract.MovieEntry.COLUMN_DESCRIPTION +
+				" TEXT NOT NULL, " +
+				MovieContract.MovieEntry.COLUMN_RATING +
+				" REAL NOT NULL, " +
+				MovieContract.MovieEntry.COLUMN_DATE +
+				" DATE NOT NULL "
+				+");";
+
+		sqLiteDatabase.execSQL(SQL_CREATE_MOVIE_TABLE);
+	}
+
+	// Upgrade database when version is changed.
+	@Override
+	public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+		Log.w(LOG_TAG, "Upgrading database from version " + oldVersion + " to " +
+				newVersion + ". OLD DATA WILL BE DESTROYED");
+		// Drop the table
+		sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + FlavorsContract.FlavorEntry.TABLE_FLAVORS);
+        sqLiteDatabase.execSQL("DELETE FROM SQLITE_SEQUENCE WHERE NAME = '" +
+                FlavorsContract.FlavorEntry.TABLE_FLAVORS + "'");
+
+		// re-create database
+		onCreate(sqLiteDatabase);
+	}
+}
